522aae47cf2cdef7164852b10b6472dd6e7ddc1a
WizelfMike <70898423+WizelfMike@users.noreply.github.com>
2020-12-01 09:37:21 +0100

    fixed a mistake of the string being there twice

--- Development/Class_Blueprint.py
+++ Development/Class_Blueprint.py
@@ -29,25 +29,3 @@ mario2._Lives = 5
 print("Mario 1's Lives: ", mario1._Lives)
 print("mario 2's Lives: ", mario2._Lives)
 
-
-class Ball(pygame.sprite.Sprite):
-    """A ball that will move across the screen
-    Returns: ball object
-    Functions: update, calcnewpos
-    Attributes: area, vector"""
-
-    def __init__(self, vector):
-        pygame.sprite.Sprite.__init__(self)
-        self.image, self.rect = load_png('ball.png')
-        screen = pygame.display.get_surface()
-        self.area = screen.get_rect()
-        self.vector = vector
-
-    def update(self):
-        newpos = self.calcnewpos(self.rect,self.vector)
-        self.rect = newpos
-
-    def calcnewpos(self,rect,vector):
-        (angle,z) = vector
-        (dx,dy) = (z*math.cos(angle),z*math.sin(angle))
-        return rect.move(dx,dy)
